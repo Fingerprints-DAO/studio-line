2c809b1ce150a66fb0e7e8b4af8a4001c8a326a8
Arod <arod.mail+fp@proton.me>
2024-02-20 12:38:50 -0300

    fix(timer): uncomment used code

--- packages/dapp-web-app/src/types/interval.ts
+++ packages/dapp-web-app/src/types/interval.ts
@@ -7,5 +7,5 @@ export enum Interval {
   CanMove = dayjs.duration(5, 'minutes').asMilliseconds(),
   Price = dayjs.duration(20, 'seconds').asMilliseconds(),
   TokenId = dayjs.duration(20, 'seconds').asMilliseconds(),
-  // Timer = dayjs.duration(1, 'minute').asMilliseconds(),
+  Timer = dayjs.duration(1, 'minute').asMilliseconds(),
 }
